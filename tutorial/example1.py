5f0bf5fed9061e97fa4f1e73e4be9a6ab4104d87
joyglitch <jli43@uoguelph.ca>
2020-07-16 11:38:35 -0400

    Initial commit

---
+++ tutorial/example1.py
@@ -0,0 +1,19 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+app = dash.Dash()
+
+app.layout = html.Div(children=[
+    html.H1('Dash Example 1'),
+    dcc.Graph(id='example',
+              figure={
+                  'data': [
+                      {'x':[1,2,3,4,5], 'y':[5,6,7,2,1], 'type':'line', 'name':'boats'},
+                      {'x':[1,2,3,4,5], 'y':[8,3,2,3,5], 'type':'bar', 'name':'cars'}
+                  ]
+              })
+])
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
---
+++ tutorial/example2-5.py
@@ -0,0 +1,8 @@
+import pandas_datareader as web
+import datetime
+
+start = datetime.datetime(2015,1,1)
+end = datetime.datetime(2018,2,8)
+
+df = web.DataReader('TSLA', 'yahoo', start, end)
+print(df.head())
